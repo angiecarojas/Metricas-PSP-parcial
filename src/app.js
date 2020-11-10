02882a4c3364b87d030a815ac0251584a7c6d1e5
angiecarojas <33725657+angiecarojas@users.noreply.github.com>
2020-11-10 12:03:22 -0500

    Terminar tabla principal

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ mongoose.connect('mongodb://localhost/crud-mongo')
 const indexRoutes = require('./routes/index');
 
 // settings
-app.set('port', process.env.PORT ||5000);
+app.set('port', process.env.PORT ||7000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
--- src/model/task.js
+++ src/model/task.js
@@ -6,7 +6,26 @@ const TaskSchema = Schema({
   RealHoras:Number,
   fecha:Date,
   Porcentaje:Number,
-
+  DplanHoras:Number,
+  DrealHoras:Number,
+  Dfecha:Date,
+  Dporcentaje:Number,
+  CplanHoras:Number,
+  CrealHoras:Number,
+  Cfecha:Date,
+  Cporcentaje:Number,
+  PplanHoras:Number,
+  PrealHoras:Number,
+  Pfecha:Date,
+  Pporcentaje:Number,
+  PMplanHoras:Number,
+  PMrealHoras:Number,
+  PMfecha:Date,
+  PMporcentaje:Number,
+  TplanHoras:Number,
+  TrealHoras:Number,
+  Tfecha:Date,
+  Tporcentaje:Number
 });
 
 module.exports = mongoose.model('tasks', TaskSchema);
